9c7f97ff595e0c3c017d88bf58625231c4a632c0
Danrleycr7 <danrleysil46@gmail.com>
2019-11-25 08:02:38 -0300

    atualizacao da pagina home

--- src/pages/estatica/home.js
+++ src/pages/estatica/home.js
@@ -1,90 +1,159 @@
 import React, { Component } from 'react';
 
-import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native';
-
+import { View, Text, TouchableOpacity, StyleSheet, Image, ScrollView } from 'react-native';
+import ImagePicker from 'react-native-image-picker';
 // import { Container } from './styles';
 import logo from '../../../assets/git.png'
 import exclamacao from '../../../assets/exclamacao.png'
 import interrogacao from '../../../assets/interrogacao.png'
 import sintomas from '../../../assets/sintomas.png'
-import supeita from '../../../assets/supeita.png'
+import suspeita from '../../../assets/supeita.png'
+import camera from '../../../assets/camera.png'
+import pontos from '../../../assets/3pontos.png'
 export default class Home extends Component {
-  static navigationOptions = {
-    headerTitle: 'Inicio'
-  };
+  state = {
+    image: null,
+    preview: null,
+    author: '',
+    place: '',
+    description: '',
+    hashtags: '',
+  }
+
+  handleSelectImage = () => {
+    ImagePicker.showImagePicker({
+      title: 'Selecione a imagem',
+    }, upload => {
+      if (upload.error) {
+        console.log('Error');
+      }
+      else if (upload.didCancel) {
+        console.log('Cancelado');
+      }
+      else {
+        const preview = {
+          uri: `data:image/jpeg;base64,${upload.data}`,
+        }
+        let prefix;
+        let ext;
+
+        if (upload.fileName) {
+          [prefix, ext] = upload.fileName.split('.')
+          ext = ext.toLowerCase() === 'heic' ? 'jpg' : ext
+        } else {
+          prefix = new Date().getTime();
+          ext = 'jpg';
+        }
+
+        const image = {
+          uri: upload.uri,
+          type: upload.type,
+          name: `${prefix}.${ext}`
+        };
+
+        this.setState({ preview, image })
+      }
+    })
+  }
+
+  static navigationOptions = ({ navigation }) => ({
+    headerLeft: (
+      <TouchableOpacity style={{ height: 20, width: 20, marginLeft: 10 }} onPress={() => navigation.navigate('Mapa')} >
+        <Image source={pontos} style={styles.iconPonto} />
+      </TouchableOpacity>
+    ),
+  });
   render() {
     return (
       <View style={styles.container}>
-        <Image
-          style={styles.logo}
-          source={logo}
-          aspectRatio={1}
-        // resizeMode='stretch'
-        />
-        <TouchableOpacity
-          style={styles.viewButton}
-          onPress={() => this.props.navigation.navigate('Doenca')}
-        >
-          <Image
-            style={styles.iconLeft}
-            source={interrogacao}
-            aspectRatio={1}
-          // resizeMode='stretch'
-          />
-          <View style={styles.info}>
-            <Text style={styles.title}>Doença</Text>
-            <Text style={styles.desc}>o que é Hanseniase?</Text>
+        <ScrollView>
+          <View style={styles.perfil}>
+            <Text style={styles.textPefil}>Olá, Danrley</Text>
           </View>
-          <Image
-            style={styles.iconRight}
-            source={exclamacao}
-            aspectRatio={1}
-          // resizeMode='stretch'
-          />
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.viewButton}>
-          <Image
-            style={styles.iconLeft}
-            source={sintomas}
-            aspectRatio={1}
-          // resizeMode='stretch'
-          />
-          <View style={styles.info}>
-            <Text style={styles.title}>Sitomas</Text>
-            <Text style={styles.desc}>Sinas sobre a doença</Text>
+          <View>
+            <ScrollView style={styles.scronHor} horizontal={true}>
+              <View style={styles.borda}>
+                {/* View lado a lado */}
+                <View style={styles.row}>
+                  <Image
+                    style={styles.iconLeft}
+                    source={interrogacao}
+                    aspectRatio={1}
+                  // resizeMode='stretch'
+                  />
+                  <Text style={styles.title}>Hanseniase</Text>
+                </View>
+                <Text style={styles.textoMini}>Doença infecciosa crônica e curável que causa, sobretudo, lesões de pele e danos aos nervos</Text>
+                {/* View lado a lado */}
+                <TouchableOpacity style={styles.buttonScrol} onPress={() => this.props.navigation.navigate('Doenca')} >
+                  <Text style={styles.textButton}>Quero ver mais</Text>
+                </TouchableOpacity>
+              </View>
+              <View style={styles.borda}>
+                {/* View lado a lado */}
+                <View style={styles.row}>
+                  <Image
+                    style={styles.iconLeft}
+                    source={sintomas}
+                    aspectRatio={1}
+                  // resizeMode='stretch'
+                  />
+                  <Text style={styles.title}>Sintomas</Text>
+                </View>
+                <Text style={styles.textoMini}>Doença infecciosa crônica e curável que causa, sobretudo, lesões de pele e danos aos nervos</Text>
+                {/* View lado a lado */}
+                <TouchableOpacity style={styles.buttonScrol}>
+                  <Text style={styles.textButton}>Quero ver mais</Text>
+                </TouchableOpacity>
+              </View>
+              <View style={styles.borda}>
+                {/* View lado a lado */}
+                <View style={styles.row}>
+                  <Image
+                    style={styles.iconLeft}
+                    source={suspeita}
+                    aspectRatio={1}
+                  // resizeMode='stretch'
+                  />
+                  <Text style={styles.title}>Suspeita</Text>
+                </View>
+                <Text style={styles.textoMini}>Doença infecciosa crônica e curável que causa, sobretudo, lesões de pele e danos aos nervos</Text>
+                {/* View lado a lado */}
+                <TouchableOpacity style={styles.buttonScrol}>
+                  <Text style={styles.textButton}>Quero ver mais</Text>
+                </TouchableOpacity>
+              </View>
+            </ScrollView>
           </View>
-          <Image
-            style={styles.iconRight}
-            source={exclamacao}
-            aspectRatio={1}
-          // resizeMode='stretch'
-          />
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.viewButton}>
-          <Image
-            style={styles.iconLeft}
-            source={supeita}
-            aspectRatio={1}
-          // resizeMode='stretch'
-          />
-          <View style={styles.info}>
-            <Text style={styles.title}>Suspeita</Text>
-            <Text style={styles.desc}>o que deve ser feito?</Text>
+          <View style={styles.HanTipos}>
+            <Text style={styles.TitleTipo}>Tipos de Hanseníase</Text>
+            <Text style={styles.textoTipo}>
+              Os sinais de hanseníase são diversos e
+              apresentam-se na pele e nos nervos periféricos.
+              Então, podemos suspeitar de hanseníase quando a
+              pessoa apresentar áreas da pele com manchas
+              esbranquiçadas, acastanhadas ou avermelhadas,
+              com alteração na sensibilidade
+            </Text>
+            <TouchableOpacity style={styles.BotaoTipo} onPress={this.handleSelectImage}>
+              <Text style={styles.textTipo}>Mais detalhes</Text>
+            </TouchableOpacity>
           </View>
-          <Image
-            style={styles.iconRight}
-            source={exclamacao}
-            aspectRatio={1}
-          // resizeMode='stretch'
-          />
-        </TouchableOpacity>
-        <View>
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => this.props.navigation.navigate('Mapa')}>
-            <Text style={styles.textButton}>Fazer Analise</Text>
-          </TouchableOpacity>
-        </View>
+          <View style={styles.viewCam}>
+            <TouchableOpacity style={styles.selectButton} onPress={this.handleSelectImage}>
+              <Image
+                style={styles.iconCamera}
+                source={camera}
+                aspectRatio={1}
+              // resizeMode='stretch'
+              />
+            </TouchableOpacity>
+            {this.state.preview && <Image style={styles.preview} source={this.state.preview} />}
+            <TouchableOpacity style={styles.butaoAnalise} onPress={this.handleSelectImage}>
+              <Text style={styles.butaoTextoAnalise}>Fazer analise</Text>
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
       </View>
     );
   }
@@ -93,65 +162,167 @@ export default class Home extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    height: 800,
-    width: null,
-    // justifyContent: 'center',
-    alignItems: 'center',
-    // backgroundColor: '#F5FCFF',
+    backgroundColor: '#f6f2f7',
+    // justifyContent: 'center'
   },
-  info: {
-    flexDirection: 'column'
-  },
-  viewButton: {
+  perfil: {
     // flex: 1,
-    flexDirection: 'row',
-    borderRadius: 10,
-    marginBottom: 20,
-    borderWidth: 2,
-    borderColor: '#CCC',
-    // borderStyle: 'dashed',
-    height: 80,
-    width: 300,
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    fontSize: 18,
-
+    width: '100%',
+    height: 100,
+    backgroundColor: '#490dbf'
   },
-  logo: {
-    width: 250,
-    height: 90,
-    marginTop: 20,
-    marginBottom: 10
-  },
-  iconLeft: {
-    width: 30,
-    height: 30,
-    marginLeft: 15,
-  },
-  iconRight: {
-    width: 30,
-    height: 30,
-    marginRight: 15,
-  },
-  title: {
-    fontSize: 18,
+  textPefil: {
+    fontSize: 20,
     fontWeight: 'bold',
+    marginTop: 50,
+    marginLeft: 10,
+    color: '#fff',
   },
-  desc: {
-    fontSize: 16
+  viewCam: {
+    flex: 1,
+    alignItems: 'center',
+    // width: '100%',
+    // backgroundColor: 'black'
   },
-  button: {
-    width: 200,
-    height: 40,
-    backgroundColor: '#5f0882',
-    borderRadius: 15,
+  scronHor: {
+    height: 170,
+    // marginTop: 80,
+    // width: 20,
+    backgroundColor: '#490dbf'
+  },
+  TitleTipo: {
+    marginTop: 10,
+    fontWeight: 'bold',
+    fontSize: 18,
+    color: '#fff',
+  },
+  textoTipo: {
+    // flex: 3,
+    // maxHeight: ,
+    textAlign: 'justify',
+    marginTop: 5,
+    marginLeft: 30,
+    marginRight: 20,
+    fontSize: 15,
+    color: '#fff'
+    // marginLeft: 50,
+  },
+  HanTipos: {
+    flex: 1,
+    height: 250,
+    width: 300,
+    marginLeft: 30,
+    backgroundColor: '#490dbf',
+    marginTop: 20,
+    borderRadius: 12,
+    alignItems: 'center'
+  },
+  borda: {
+    borderRadius: 8,
+    // borderWidth: 2,
+    // borderColor: '#CCC',
+    width: 150,
+    height: 150,
     alignItems: 'center',
     justifyContent: 'center',
-    marginTop: 15,
+    marginTop: 8,
+    margin: 10,
+    backgroundColor: '#fff'
+  },
+  textBoder: {
+    color: '#CCC'
+  },
+  textoMini: {
+    textAlign: 'center',
+    marginBottom: 17,
+    fontSize: 12,
+  },
+  iconLeft: {
+    width: 20,
+    height: 25,
+    marginRight: 5,
+    marginLeft: -5,
+    // marginTop: -100
+  },
+  title: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    // marginRight: 30,
+  },
+  buttonScrol: {
+    backgroundColor: '#310975',
+    borderRadius: 8,
+    height: 23,
+    width: 100,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 5,
   },
   textButton: {
     color: '#fff',
-    fontWeight: 'bold',
+    fontSize: 12,
+  },
+  row: {
+    flex: 1,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginTop: 15,
+    // alignItems: 'center'
+  },
+  selectButton: {
+    borderRadius: 8,
+    borderWidth: 3,
+    borderColor: '#490dbf',
+    height: 42,
+    width: 300,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 20,
+    marginBottom: 20,
+
+  },
+
+  butaoTextoAnalise: {
+    fontSize: 16,
+    color: '#fff',
+  },
+  iconCamera: {
+    width: 30,
+    height: 30
+  },
+  iconPonto: {
+    width: 25,
+    height: 20
+  },
+  butaoAnalise: {
+    width: 300,
+    height: 40,
+    backgroundColor: '#490dbf',
+    borderRadius: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 30,
+  },
+  BotaoTipo: {
+    width: 150,
+    height: 30,
+    backgroundColor: '#fff',
+    borderRadius: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 23,
+  },
+  textTipo: {
     fontSize: 14,
+    color: 'black',
+  },
+  preview: {
+    width: 100,
+    height: 100,
+    // marginTop: 5,
+    marginBottom: 10,
+    alignSelf: 'center',
+    borderRadius: 4,
   },
 })
